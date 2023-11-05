31c1776fe2623053a06a819123aad14ea1f4eaae
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-11-05 00:46:31 -0400

    check DBNAME environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,9 @@ import { JoiValidationSchema } from './common/config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: process.env.DBNAME,
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
--- src/common/config/joi.validation.ts
+++ src/common/config/joi.validation.ts
@@ -2,6 +2,7 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
   MONGODB: Joi.required(),
+  DBNAME: Joi.required(),
   PORT: Joi.number().default(3005),
   DEFAULT_LIMIT: Joi.number().default(6),
 });
