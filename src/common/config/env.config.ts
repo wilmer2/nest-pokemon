5961c0564d6fe4b682acfc1fe26d3a4b5a0ca8f3
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-11-05 01:13:13 -0400

    add DBNAME to injection config class

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -2,6 +2,7 @@
 export const EnvConfiguration = () => ({
   environment: process.env.NODE_ENV || 'dev',
   mongodb: process.env.MONGODB,
+  dbname: process.env.DBNAME,
   port: process.env.PORT || 3002,
   defaultLimit: +process.env.DEFAULT_LIMIT || 7,
 });
