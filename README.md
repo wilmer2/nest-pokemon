16f7082a36475891c3744bf7ce2f288503779cbd
Wilmer Maldonado <wilmer.payall@gmail.com>
2023-11-04 23:46:00 -0400

    validate environment variables

--- README.md
+++ README.md
@@ -23,7 +23,17 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
-5. Reconstruir la base de datos con la semilla
+5. Clonar el archivo **.env.template** y renombrar la copia **.env**
+
+6. LLenar la variables de entorno definidas en `.env`
+
+7. Ejecuta la aplicacion en dev:
+
+```
+yarn start:dev
+```
+
+8. Reconstruir la base de datos con la semilla
 
 ```
 http://localhost:3000/api/v2/seed
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,13 @@ import { CommonModule } from './common/common.module';
 import { EnvConfiguration } from './common/config/env.config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
+import { JoiValidationSchema } from './common/config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
